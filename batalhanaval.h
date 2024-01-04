c67b85435a852f5bf504faa6c4afb05982eabe6b
cpedrohenrique1 <phpedrohenrique474@gmail.com>
2024-01-04 19:01:05 +0000

    atualizacoes batalhanaval.h

--- batalhanaval.h
+++ batalhanaval.h
@@ -3,140 +3,173 @@
 
 #include "campodebatalha.h"
 
-class BatalhaNaval{
+class BatalhaNaval
+{
 private:
-    CampoDeBatalha *campo;
+    CampoDeBatalha *campo_setado;
+    CampoDeBatalha *campo_player;
     int submarinos;
     int destroyers;
     int cruzares;
+
 public:
-    BatalhaNaval(int tamanho = 14) : submarinos(0), destroyers(0), cruzares(0){
-        try{
-            campo = new CampoDeBatalha(tamanho);
-        }catch(std::bad_alloc&){
+    BatalhaNaval(int tamanho = 14) : submarinos(0),
+                                     destroyers(0),
+                                     cruzares(0),
+                                     campo_player(0),
+                                     campo_setado(0)
+    {
+        try
+        {
+            campo_setado = new CampoDeBatalha(tamanho);
+        }
+        catch (std::bad_alloc &)
+        {
             throw std::string("Memoria insuficiente");
         }
     }
-    ~BatalhaNaval(){
-        if (campo){
-            delete campo;
+    ~BatalhaNaval()
+    {
+        if (campo_setado)
+        {
+            delete campo_setado;
         }
     }
-    void limparConsole(){
+    void limparConsole()
+    {
         system("clear");
     }
-    void desenharMenu(){
+    void desenharMenu()
+    {
         limparConsole();
         std::cout << "\tBatalha Naval" << std::endl;
         std::cout << "\t1 - Jogar" << std::endl;
         std::cout << "\t2 - Creditos" << std::endl;
         std::cout << "\t3 - Sair" << std::endl;
     }
-    void desenharMatriz()const{
-        if (!campo){
+    void desenharMatriz() const
+    {
+        if (!campo_setado)
+        {
             throw std::string("Campo de batalha nao existe");
         }
-        for(int i = 0; i < campo->getTamanho(); i++){
+        for (int i = 0; i < campo_setado->getTamanho(); i++)
+        {
             std::cout << "Linha " << i + 1 << "\t";
-            for(int j = 0; j < campo->getTamanho(); j++){
-                std::cout << campo->getMatriz(i, j) << " ";
+            for (int j = 0; j < campo_setado->getTamanho(); j++)
+            {
+                std::cout << campo_setado->getMatriz(i, j) << " ";
             }
             std::cout << std::endl;
         }
     }
-    int lerQuantidade(std::string entrada){
+    int lerQuantidade(std::string entrada)
+    {
         std::cout << "Insira a quantidade de " + entrada << ': ';
         int variavel;
         std::cin >> variavel;
         return variavel;
     }
-    int lerCoordenadas(){
+    int lerCoordenadas()
+    {
         int coordenada;
         std::cin >> coordenada;
-        if (coordenada <= 0 || coordenada > campo->getTamanho()){
+        if (coordenada <= 0 || coordenada > campo_setado->getTamanho())
+        {
             throw std::string("Linha invalida");
         }
         return coordenada;
     }
 
-    void setSubmarinos(int submarino){
-        if (submarino < 0){
+    void setSubmarinos(int submarino)
+    {
+        if (submarino < 0)
+        {
             throw std::string("Nao pode ser negativo");
         }
         this->submarinos = submarino;
     }
-    void setDestroyers(int destroyer){
-        if (destroyer < 0){
+    void setDestroyers(int destroyer)
+    {
+        if (destroyer < 0)
+        {
             throw std::string("Nao pode ser negativo");
         }
         this->destroyers = destroyer;
     }
-    void setCruzares(int cruzados){
-        if (cruzados < 0){
+    void setCruzares(int cruzados)
+    {
+        if (cruzados < 0)
+        {
             throw std::string("Nao pode ser negativo");
         }
         this->cruzares = cruzados;
     }
-    void jogar(){
-        campo->resetMatriz();
+    void jogar()
+    {
+        if (!campo_setado){
+            throw std::string("Campo nao foi setado");
+        }
+        campo_setado->resetMatriz();
         limparConsole();
         setSubmarinos(lerQuantidade("Submarinos"));
         setDestroyers(lerQuantidade("Destroyers"));
         setCruzares(lerQuantidade("Cruzares"));
         int linha, coluna;
-        for (int i = 0; i < submarinos; ++i){
+        for (int i = 0; i < submarinos; ++i)
+        {
             limparConsole();
             desenharMatriz();
             std::cout << "Insira a linha do submarino " << i + 1 << ": ";
             linha = lerCoordenadas();
             std::cout << "Insira a coluna do submarino " << i + 1 << ": ";
             coluna = lerCoordenadas();
-            campo->setMatriz(linha, coluna, 'S');
+            campo_setado->setMatriz(linha, coluna, 'S');
         }
-        for (int i = 0; i < destroyers; ++i){
+        for (int i = 0; i < destroyers; ++i)
+        {
             limparConsole();
             desenharMatriz();
             std::cout << "Insira a linha do destroyer " << i + 1 << ": ";
             linha = lerCoordenadas();
             std::cout << "Insira a coluna do destroyer " << i + 1 << ": ";
             coluna = lerCoordenadas();
-            campo->setMatriz(linha, coluna, 'D');
+            campo_setado->setMatriz(linha, coluna, 'D');
         }
-        for (int i = 0; i < cruzares; ++i){
+        for (int i = 0; i < cruzares; ++i)
+        {
             limparConsole();
             desenharMatriz();
             std::cout << "Insira a linha do cruzeiro " << i + 1 << ": ";
             linha = lerCoordenadas();
             std::cout << "Insira a coluna do cruzeiro " << i + 1 << ": ";
             coluna = lerCoordenadas();
-            campo->setMatriz(linha, coluna, 'C');
+            campo_setado->setMatriz(linha, coluna, 'C');
         }
         limparConsole();
         desenharMatriz();
-        std::cout << "Matriz esta correta? se nao insira 1 para reseta-la: ";
-        int entrada;
-        std::cin >> entrada;
-        if (entrada == 1){
-            
-        }
     }
-    void creditos(){
+    
+    void creditos()
+    {
         std::cout << "Desenvolvido por: \n\tPedro Henrique Brito da Silva Miranda" << std::endl;
     }
-    void start(){
+    void start()
+    {
         desenharMenu();
         int opcao;
         std::cout << "Digite a opcao: ";
         std::cin >> opcao;
-        switch (opcao){
-            case 1:
-                jogar();
-                break;
-            case 2:
-                creditos();
-                break;
-            default:
-                break;
+        switch (opcao)
+        {
+        case 1:
+            jogar();
+            break;
+        case 2:
+            creditos();
+            break;
+        default:
+            break;
         }
     }
 };
